639b3d7bcfa2adbf0ba5d734019d3328e26c9a94
DMB29 <mbilaldogan90@gmail.com>
2022-03-08 12:33:32 +0300

    değişken

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import Header from "./component/header";
 
+const name = "Mustafa Bilal"
+const surname = "DOĞAN"
 function App() {
   return (
     <div>
       <h1>Merhaba Bilal</h1>
       <Header/>
       <Header></Header>
+      <h1>{name}{surname}</h1>
+      <h1>{`benim adım ${name},  soyadım ${surname}`}</h1>
 
       
     </div>
